143e697851f0df9fb8247b0fd8bf9bfa5c494ea5
Hector Bello Martinez <62455718+hbelloma@users.noreply.github.com>
2021-04-14 21:00:28 -0500

    Update SimpleBlockchainexample.py

--- SimpleBlockchainexample.py
+++ SimpleBlockchainexample.py
@@ -1,6 +1,17 @@
 class Blockchain (object):  #The blockchain
 	def __init__(self):
 		self.chain = [];
+		
+	def getLastBlock(self):
+        	return self.chain[-1];
+    	def addBlock(self, block):
+        	if(len(self.chain) > 0):
+            		block.prev = self.getLastBlock().hash;
+        	else:
+            		block.prev = "none";
+        	self.chain.append(block)
+	
+	
 class Block (object):     #The block
 	def __init__(self, transactions, time, index):
 		self.index = index; #Block number 
@@ -17,7 +28,7 @@ class Block (object):     #The block
 
 		for transaction in self.transactions:
 			hashTransactions += transaction.hash;
-		hashString = str(self.time) + hashTransactions + self.gym + self.prev + str(self.nonse);
+		hashString = str(self.time) + hashTransactions + self.prev + str(self.index);
 		hashEncoded = json.dumps(hashString, sort_keys=True).encode();
 		return hashlib.sha256(hashEncoded).hexdigest();  #SHA256 Hash encoding -same as bitcoin
   
